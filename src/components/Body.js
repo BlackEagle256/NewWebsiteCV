e07ea48d301c25846e4462a341aa34844484d3ab
mohammadHosein <mohammadhoseindadgostr@gmail.com>
2025-07-20 19:00:43 +0330

    Header Section Completed

--- src/components/Body.js
+++ src/components/Body.js
@@ -3,24 +3,36 @@ import React from 'react';
 export default function Body() {
     return (
         <main className="relative w-full overflow-hidden">
-            <div className="relative bg-orangeCustom h-[700px] w-full">
-                <svg
-                    className="absolute right-0 top-0 h-full w-full z-10 bg-red-700"
-                    viewBox="0 -20 200 300"
-                    preserveAspectRatio="none"
-                >
-                    <path
-                        d="M0,105 C0,100 200,450 400,-500 L500,500 L0,500 Z"
-                        fill="white"
-                    />
-                </svg>
+            <div className="relative w-full h-[700px] bg-white">
+                <div className="absolute inset-0 z-0">
+                    <svg
+                        id="wave"
+                        className="rotate-180 transition duration-300"
+                        viewBox="0 0 1440 490"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <path
+                            className="translate-y-0 opacity-100"
+                            fill="#F18519"
+                            d="M0,294L60,310.3C120,327,240,359,360,343C480,327,600,261,720,204.2C840,147,960,98,
+                            1080,73.5C1200,49,1320,49,1440,73.5C1560,98,1680,147,1800,204.2C1920,261,2040,327,
+                            2160,318.5C2280,310,2400,229,2520,171.5C2640,114,2760,82,2880,89.8C3000,98,3120,147,
+                            3240,212.3C3360,278,3480,359,3600,351.2C3720,343,3840,245,3960,196C4080,147,4200,147,
+                            4320,147C4440,147,4560,147,4680,138.8C4800,131,4920,114,5040,138.8C5160,163,5280,229,
+                            5400,277.7C5520,327,5640,359,5760,359.3C5880,359,6000,327,6120,294C6240,261,6360,229,
+                            6480,196C6600,163,6720,131,6840,155.2C6960,180,7080,261,7200,285.8C7320,310,7440,278,
+                            7560,253.2C7680,229,7800,212,7920,187.8C8040,163,8160,131,8280,114.3C8400,98,8520,98,
+                            8580,98L8640,98L8640,490L0,490Z"
+                        />
+                    </svg>
+                </div>
 
-                <section className="relative z-20 p-10 max-w-xl">
+                <section className="relative z-20 p-16 max-w-xl">
                     <h2 className="text-3xl font-semibold text-white">Hello, I Am</h2>
                     <h2 className="text-3xl font-semibold text-white">Mohammad Hossein</h2>
                     <h2 className="text-3xl font-semibold mb-6 text-white">Dadgostar Nejhad</h2>
-                    <p className="text-[#FCE0C5] text-md mt-2 w-80">
-                        A FrontEnd Developer With over 3 years of experience in designing and implementing modern user interfaces.
+                    <p className="text-md text-[#FCE0C5] mt-2 w-80">
+                        A FrontEnd Developer with over 3 years of experience in designing and implementing modern user interfaces.
                     </p>
                 </section>
             </div>
